4ec1ad3bbbb0daa33829bd0d21634ff894554d55
Donovan Daniels <hewwo@yiff.rocks>
2023-12-12 12:23:05 -0600

    allow spaces in artist names

--- app/logical/printable_string_validator.rb
+++ app/logical/printable_string_validator.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class PrintableStringValidator < ActiveModel::EachValidator
-  REGEX = /^[0-9a-zA-Z_.+()-]*$/
+  REGEX = /^[0-9a-zA-Z_.+()\-[:space:]]*$/
 
   def validate_each(record, attribute, value)
     return if REGEX.match? value
 
-    record.errors.add(attribute, "'#{value}' can only contain alphanumerics and _.-+()")
+    record.errors.add(attribute, "'#{value}' can only contain alphanumerics, spaces, and _.-+()")
   end
 end
