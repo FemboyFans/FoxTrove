1a26871ccd011fa84a0dccf465f56833cbaefef9
Donovan Daniels <hewwo@yiff.rocks>
2023-12-17 01:02:13 +0000

    Add live polling updates on stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -15,6 +15,26 @@ class StatsController < ApplicationController
     ).joins(artist_submission: { artist_url: :artist }).group(:site_type).index_by(&:site_type)
     @counts.transform_keys! { |id| ArtistUrl.site_types.invert[id] }
     @definitions = Sites.definitions.sort_by(&:display_name)
+    respond_to do |fmt|
+      fmt.html
+      fmt.json do
+        render json: {
+          artist_urls: render_to_string(partial: "artist_urls/list", formats: %i[html], locals: { artist_urls: ArtistUrl.where(id: helpers.job_stats.active_urls) }),
+          scraped: @counts,
+          storage: {
+            db: helpers.number_to_human_size(@db_size),
+            original: helpers.number_to_human_size(@original_size),
+            samples: helpers.number_to_human_size(@sample_size),
+          },
+          jobs: {
+            scraping: helpers.job_stats.scraping_queued.values.sum,
+            file_downloads: helpers.job_stats.submission_download_queued.values.sum,
+            iqdb: helpers.job_stats.e6_iqdb_queued.values.sum,
+            post_sync: helpers.job_stats.e621_sync_queued.values.sum,
+          },
+        }
+      end
+    end
   end
 
   private
