d44de870e75e57dec1c6a79ce292915fb2956707
Donovan Daniels <hewwo@yiff.rocks>
2025-01-14 15:51:03 -0600

    make fa icon replacement non-greedy

--- app/models/artist_submission.rb
+++ app/models/artist_submission.rb
@@ -10,4 +10,13 @@ class ArtistSubmission < ApplicationRecord
   def self.for_site_with_identifier(site:, identifier:)
     joins(:artist_url).find_by(identifier_on_site: identifier, artist_url: { site_type: site })
   end
+
+  def update_fa!
+    html = artist_url.scraper.send(:get_submission_html, identifier_on_site)
+    title = html.css(".submission-title").first&.content&.strip
+    description = html.css(".submission-description").first&.content&.strip
+    self.title_on_site = title if title.present?
+    self.description_on_site = description if description.present?
+    save!
+  end
 end
