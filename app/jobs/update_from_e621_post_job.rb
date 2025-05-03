8e5488f472731d211ce62bef611338ae4f2dbc09
Donovan Daniels <hewwo@yiff.rocks>
2025-05-03 13:30:20 -0500

    Fix job for api changes

--- app/jobs/update_from_e621_post_job.rb
+++ app/jobs/update_from_e621_post_job.rb
@@ -7,7 +7,7 @@ class UpdateFromE621PostJob < ApplicationJob
     create_e6_post(sub, post) if sub.present?
     return unless VALID_EXTENSIONS.include?(post.dig("file", "ext"))
 
-    sample = post["samples"].find { |s| s["type"] == "large" }
+    sample = post["variants"].find { |s| s["type"] == "large" }
     sample_url = sample&.[]("url") || post.dig("file", "url")
     results = IqdbProxy.query_url(sample_url)
 
